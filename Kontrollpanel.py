56f7572be8a53b4c1087859ed288d7c8e3daab21
Laborprojekt <lejonina@tutanota.com>
2025-04-14 00:41:07 +0200

    Einbindung AHT und Datenbank

--- Kontrollpanel.py
+++ Kontrollpanel.py
@@ -14,24 +14,27 @@ Hardware:
 - ST7789V3, 1.69", 240x280p
 '''
 
-from machine import Pin
+from machine import Pin, SoftI2C
 import utime
 import ujson
 import network
 from umqtt.simple import MQTTClient
+import aht10
 
 #---------------- Pin ----------------#
 
-rl_reboot = Pin(9,Pin.OUT)    #NC
-rl_schuko_1 = Pin(10, Pin.OUT) #NO
-rl_schuko_2 = Pin(11, Pin.OUT) #NO
-rl_schuko_3 = Pin(12, Pin.OUT) #NO
-rl_schuko_4 = Pin(13, Pin.OUT) #NO
-rl_schuko_5 = Pin(14, Pin.OUT) #NO
+rl_reboot = Pin(14,Pin.OUT)    #NC
+rl_schuko_1 = Pin(13, Pin.OUT) #NO
+rl_schuko_2 = Pin(12, Pin.OUT) #NO
+rl_schuko_3 = Pin(11, Pin.OUT) #NO
+rl_schuko_4 = Pin(10, Pin.OUT) #NO
+rl_schuko_5 = Pin(9, Pin.OUT) #NO
 
 schalter_display= Pin (35, Pin.IN, Pin.PULL_DOWN)
 taster_schuko12 = Pin(37, Pin.IN, Pin.PULL_DOWN)
 taster_schuko34 = Pin(39, Pin.IN, Pin.PULL_DOWN)
+
+i2c = SoftI2C(sda = 41, scl = 42)
 
 
 #---------------- Variablen ----------------#
@@ -50,25 +53,35 @@ time_irq_schuko34 = 0
 
 shelly_energy = {}
 
+# Sensor
+aht = aht10.AHT10(i2c = i2c)
+
+# Allgemein
+temp_list = []
+hum_list = []
+last_aht_check = 0
+INTERVALL_MS_AHT = 10000
+
+INTERVALL_MS_TASTER = 2000
+
 #---------------- MQTT-Konfiguration ----------------#
-#MQTT_SERVER = "192.168.188.26"   #Achtung: aktuelle Adresse des Brokers!
-MQTT_SERVER = "10.10.14.20"
+MQTT_SERVER = "192.168.188.26"   #Achtung: aktuelle Adresse des Brokers!
 CLIENT_ID_relais = "ESP_Relais"
 CLIENT_ID_shelly = "ESP_Shelly"
+CLIENT_ID_ESP = "ESP_send"
 MQTT_TOPIC_relais = "dashboard_switch"
 MQTT_TOPIC_shelly = "shelly"
+MQTT_TOPIC_AHT = "AHT"
 
 # @home
-#WIFI_SSID = "502-Bad-Gateway"
-#WIFI_PASSWORD = "66813838796323588312"
-
-# @hoeb
-WIFI_SSID = "WLAN-HOEB-Gast"
-WIFI_PASSWORD = "hoeb202344"
+WIFI_SSID = "502-Bad-Gateway"
+WIFI_PASSWORD = "66813838796323588312"
 
 # @BZTG
 # WIFI_SSID = "BZTG-IoT" 
 # WIFI_PASSWORD = "WerderBremen24"
+
+BAUGRUPPE = "Kontrollpanel"
 
 #---------------- Funktion zur Datenauswertung ------------- 
 def sub_relais(topic, msg):
@@ -133,13 +146,28 @@ def sub_shelly(topic, msg):
     shelly_energy = daten
     
     print(shelly_energy)
+
+
+def mqtt_publish(json_data, topic):
+    
+    # MQTT-Broker Adresse
+    client = MQTTClient(CLIENT_ID_ESP, MQTT_SERVER, port=1883)
+    
+    client.connect()  # Verbindung zum Broker aufbauen
+    print("Verbunden mit MQTT-Broker")
+
+    # Nachricht an Topic senden
+    client.publish(topic, json_data)
+    print(f"Nachricht veröffentlicht: {json_data} an Topic: {topic}")
+
+    client.disconnect()
     
 
 def schuko12_irq(pin):
     global schuko12, schuko1, schuko2, time_irq_schuko12
     timestamp = utime.ticks_ms()
 
-    if timestamp - time_irq_schuko12 >= 1000:
+    if timestamp - time_irq_schuko12 >= INTERVALL_MS_TASTER:
         print("schuko12 interrupt")
         
         schuko12 = not schuko12
@@ -153,7 +181,7 @@ def schuko34_irq(pin):
     global schuko34, schuko3, schuko4, time_irq_schuko34
     timestamp = utime.ticks_ms()
     
-    if timestamp - time_irq_schuko12 >= 1000:
+    if timestamp - time_irq_schuko12 >= INTERVALL_MS_TASTER:
         print("schuko34 interrupt")
         
         schuko34 = not schuko34
@@ -162,7 +190,48 @@ def schuko34_irq(pin):
         schuko4 = schuko34
         
         time_irq_schuko34 = timestamp
+        
+def mittelwert (value, value_list):
+    value_sum = 0
     
+    value_list.append(value)
+    
+    len_list = len(value_list)
+    
+    if len_list > 10:
+        value_list.pop(0)
+    
+    copy_list = value_list[:]
+    
+    if len_list >= 3:
+        copy_list.sort
+        copy_list.pop(0)
+        copy_list.pop()
+    
+    for i in copy_list:  
+        value_sum += i
+        
+    mittelwert = int(value_sum/len(copy_list))
+     
+    return mittelwert, value_list
+
+def store_data(timestamp, mid_temp, mid_hum):
+
+    json_raw = {
+            "Baugruppe" : BAUGRUPPE,
+            "timestamp" : timestamp,
+            "Sensorwerte" :
+                {
+                "temp" : mid_temp,
+                "hum" : mid_hum,
+                }
+            }   
+    
+    json_data = ujson.dumps(json_raw)
+
+    return json_data
+    
+
 #---------------- Funktion WIFI -------------------------------------------
 def connectWIFI(): 
     wlan = network.WLAN(network.STA_IF)
@@ -210,5 +279,25 @@ while True:
     rl_schuko_2.value(schuko2)
     rl_schuko_3.value(schuko3)
     rl_schuko_4.value(schuko4)
-    
+
+#---------------- Relais schalten -----------------------------------------
+    if utime.ticks_ms() - last_aht_check >= INTERVALL_MS_AHT:
+        
+        last_aht_check = utime.ticks_ms()
+        # Messwerte erheben
+        temp = aht.temperature()
+        hum = aht.humidity()
+        
+        clock = utime.localtime()
+        timestamp = f"{clock[0]}-{clock[1]}-{clock[2]}T{clock[3]}:{clock[4]}:{clock[5]}_MEZ"
+        
+        # Mittelwerte bilden 
+        mid_temp, temp_list = mittelwert(temp, temp_list)
+        mid_hum, hum_list = mittelwert(hum, hum_list)
+        
+        json_data = store_data(timestamp, mid_temp, mid_hum)
+        
+        mqtt_publish(json_data, MQTT_TOPIC_AHT)
+        print("AHT data send")
+     
 
